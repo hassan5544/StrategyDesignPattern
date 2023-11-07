c817b1a8c4c189361b34b29e46d2318803b26855
hassan5544 <hmh.lol55@gmail.com>
2023-11-07 21:20:03 +0300

    Add project files.

---
+++ StrategyDesignPattern/PayingToCompany.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDesignPattern
+{
+    public class PayingToCompany
+    {
+
+        private readonly IPaymentMethodcs _paymentMethods;
+
+        public PayingToCompany(IPaymentMethodcs paymentMethodcs)
+        {
+            _paymentMethods = paymentMethodcs;
+        }
+        public void Pay(decimal Amount)
+        {
+            _paymentMethods.PayMoney(Amount);
+        }
+    }
+}
---
+++ StrategyDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using StrategyDesignPattern;
+
+class Program
+{
+    public static void Main()
+    {
+        PayingToCompany payment = new PayingToCompany(new MasterCardMethod());
+
+        payment.Pay(1000);
+
+
+        PayingToCompany payment2 = new PayingToCompany(new PayPalMethod());
+
+        payment2.Pay(1000);
+    }
+}
